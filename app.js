2de3d7a3ca2f9737a8a2cb48055de82f54b7ee68
Plinio Araujo <plinioglad@gmail.com>
2023-06-11 16:19:22 -0300

    Pokedex Atualizado - Vídeo 43:19

--- app.js
+++ app.js
@@ -1,29 +1,94 @@
-const fetchPokemon = () =>{
-  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-
-  const pokemonPromises = []
-
-  for (let i = 1; i < 150; i++) {
-    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-  }
-
-  Promise.all(pokemonPromises)
-    .then(pokemons => {
-      
-      const lisPokemons = pokemons.reduce((acc,pokemon)=>{
-        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-        acc += `
-        <li class="card">
-          <img class="card-image ${types[0]}" alt="${pokemon.name}" src="" />
-          <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-          <p class="card-subtitle">${types.join(' | ')}</p>
-        </li>`
-        return acc
-      },0)
-
-      console.log(lisPokemons)
-
-    })
+const getTypeColor = type => {
+  const normal = '#F5F5F5'
+  return {
+    normal,
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    ice: '#DEF3FD',
+    water: '#DEF3FD',
+    ground: '#F4E7DA',
+    rock: '#D5D5D4',
+    fairy: '#FCEAFF',
+    poison: '#98D7A5',
+    bug: '#F8D5A3',
+    ghost: '#CAC0F7',
+    dragon: '#97B3E6',
+    psychic: '#EAEDA1',
+    fighting: '#E6E0D4'
+  }[type] || normal
 }
 
-fetchPokemon()
+const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=15"
+
+const getOnlyFulfilled = async ({func, arr}) =>{
+  const promises = arr.map(func)
+  const responses = await Promise.allSettled(promises)
+  return responses.filter(response => response.status == 'fulfilled')
+}
+
+const getPokemonsType = async pokeApiResults => {
+ const fulfilled = await getOnlyFulfilled({ arr: pokeApiResults, func: result => fetch(result.url)})
+  const pokePromises = fulfilled.map(url => url.value.json())
+  const pokemons = await Promise.all(pokePromises)
+  return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
+}
+
+
+const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
+  const urlAsArray = url.split('/')
+  return urlAsArray.at(urlAsArray.length - 2)
+})
+
+const getPokemonsImgs = async ids => {
+  const fulfilled = await getOnlyFulfilled({arr: ids, func: id => fetch(`./assets/img/${id}.png`)})
+  return fulfilled.map(response => response.value.url)
+}
+
+const getPokemons = async () =>{
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter as informações')
+    }
+    
+    const { results: pokeApiResults } = await response.json()
+    const types = await getPokemonsType(pokeApiResults)
+    const ids = getPokemonsIds(pokeApiResults)
+    const imgs = await getPokemonsImgs(ids)
+    const pokemons = ids.map((id, i) => ({id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgs[i]}))
+    
+    return pokemons
+
+  } catch (error) {
+    console.log("Algo deu errado!" + error)
+  }
+}
+
+const renderPokemons = pokemons => {
+  const ul = document.querySelector('[data-js="pokemons-list"]')
+  pokemons.forEach( ({id, name, types, imgUrl}) => {
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    const nameContainer = document.createElement('h2')
+    const typeContainer = document.createElement('p')
+    const [firstType] = types
+
+    img.setAttribute('src', imgUrl)
+    img.setAttribute('alt', name)
+    li.setAttribute('class', `card ${firstType}`)
+    li.style.setProperty('--type-color', getTypeColor(firstType))
+
+
+    console.log(li)
+  });  
+
+
+}
+const handlePageLoaded = async () => {
+ const pokemons = await getPokemons()
+ renderPokemons(pokemons)
+}
+
+handlePageLoaded()
