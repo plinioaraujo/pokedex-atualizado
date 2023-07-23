0ae87ec4c57aa49b92d5f5c84dba1c897c9e1043
Plinio Araujo <plinioglad@gmail.com>
2023-07-23 10:59:28 -0300

    Pokemon atualizado

--- app.js
+++ app.js
@@ -21,44 +21,37 @@ const getTypeColor = type => {
 
 const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=15"
 
-const getOnlyFulfilled = async ({func, arr}) =>{
-  const promises = arr.map(func)
-  const responses = await Promise.allSettled(promises)
-  return responses.filter(response => response.status == 'fulfilled')
-}
+const paginationInfo = (() =>{
+  //IIFE
+  const limit = 15
+  let offset = 0 
+  
+  const getLimit = () => limit
+  const getOffset = () => offset
+  const incrementOffset = () => offset += limit
 
-const getPokemonsType = async pokeApiResults => {
- const fulfilled = await getOnlyFulfilled({ arr: pokeApiResults, func: result => fetch(result.url)})
-  const pokePromises = fulfilled.map(url => url.value.json())
-  const pokemons = await Promise.all(pokePromises)
-  return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
-}
+  return { getLimit, getOffset, incrementOffset }
 
+})()
 
-const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
-  const urlAsArray = url.split('/')
-  return urlAsArray.at(urlAsArray.length - 2)
-})
-
-const getPokemonsImgs = async ids => {
-  const fulfilled = await getOnlyFulfilled({arr: ids, func: id => fetch(`./assets/img/${id}.png`)})
-  return fulfilled.map(response => response.value.url)
-}
-
-const getPokemons = async () =>{
+const getPokemons = async () => {
   try {
-    const response = await fetch(url)
+
+    const { getLimit, getOffset, incrementOffset } = paginationInfo
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${getLimit()}&offset=${getOffset()}`)
 
     if (!response.ok) {
       throw new Error('Não foi possível obter as informações')
     }
-    
+
     const { results: pokeApiResults } = await response.json()
     const types = await getPokemonsType(pokeApiResults)
     const ids = getPokemonsIds(pokeApiResults)
     const imgs = await getPokemonsImgs(ids)
-    const pokemons = ids.map((id, i) => ({id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgs[i]}))
-    
+    const pokemons = ids.map((id, i) => ({ id, name: pokeApiResults[i].name, types: types[i], imgUrl: imgs[i] }))
+
+    incrementOffset ()
     return pokemons
 
   } catch (error) {
@@ -66,10 +59,36 @@ const getPokemons = async () =>{
   }
 }
 
+const getOnlyFullfilled = async ({ func, arr }) => {
+  const promises = arr.map(func)
+  const responses = await Promise.allSettled(promises)
+  return responses.filter(response => response.status == 'fulfilled')
+}
+
+const getPokemonsType = async pokeApiResults => {
+  const fulfilled = await getOnlyFullfilled({ arr: pokeApiResults, func: result => fetch(result.url) })
+  const pokePromises = fulfilled.map(url => url.value.json())
+  const pokemons = await Promise.all(pokePromises)
+  return pokemons.map(fulfilled => fulfilled.types.map(info => DOMPurify.sanitize(info.type.name)))
+}
+
+const getPokemonsIds = pokeApiResults => pokeApiResults.map(({ url }) => {
+  const urlAsArray = DOMPurify.sanitize(url).split('/')
+  return urlAsArray[urlAsArray.length - 2]
+})
+
+const getPokemonsImgs = async ids => {
+  const fulfilled = await getOnlyFullfilled({ arr: ids, func: id => fetch(`./assets/img/${id}.png`) })
+  return fulfilled.map(response => response.value.url)
+}
+
+
+
 const renderPokemons = pokemons => {
-  
   const ul = document.querySelector('[data-js="pokemons-list"]')
-  pokemons.forEach( ({id, name, types, imgUrl}) => {
+  const fragment = document.createDocumentFragment()
+
+  pokemons.forEach(({ id, name, types, imgUrl }) => {
     const li = document.createElement('li')
     const img = document.createElement('img')
     const nameContainer = document.createElement('h2')
@@ -78,18 +97,51 @@ const renderPokemons = pokemons => {
 
     img.setAttribute('src', imgUrl)
     img.setAttribute('alt', name)
+    img.setAttribute('class', 'card-image')
     li.setAttribute('class', `card ${firstType}`)
     li.style.setProperty('--type-color', getTypeColor(firstType))
 
+    nameContainer.textContent = `${id}. ${name[0].toUpperCase()}${name.slice(1)}`
+    typeContainer.textContent = types.length > 1 ? types.join(' | ') : firstType
+    li.append(img, nameContainer, typeContainer)
 
-    console.log(li)
-  });  
+    fragment.append(li)
 
+  });
 
+  ul.append(fragment)
 }
+
+const observerLastPokemon = pokemonsObserver => {
+  const lastPokemon = document.querySelector('[data-js="pokemons-list"]').lastChild
+  pokemonsObserver.observe(lastPokemon)
+}
+
+const handleNextPokemonsRender = () => {
+  const pokemonsObserver = new IntersectionObserver(async ([lastPokemon],observer) => {
+
+    if (!lastPokemon.isIntersecting) {
+      return
+    }
+    
+    observer.unobserve(lastPokemon.target)
+
+    if (paginationInfo.getOffset() === 150) {
+      return
+    }
+    const pokemoms = await getPokemons()
+    renderPokemons(pokemoms)
+   
+    observerLastPokemon(pokemonsObserver)
+  }, {rootMargin: '500px'})
+
+  observerLastPokemon(pokemonsObserver)
+}
+
 const handlePageLoaded = async () => {
- const pokemons = await getPokemons()
- renderPokemons(pokemons)
+  const pokemons = await getPokemons()
+  renderPokemons(pokemons)
+  handleNextPokemonsRender()
 }
 
 handlePageLoaded()
